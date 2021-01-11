6f97b462fdf58ed1525594826c2c0b1a01e1ea3b
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-01-12 11:34:57 +1300

    Artwork pages working

--- server.js
+++ server.js
@@ -22,3 +22,11 @@ server.get('/home', (req, res) => {
   res.render(template, viewData)
 
 })
+
+server.get('/artworks/:id', (req, res) => {
+  const viewData = {
+    title: art.find(element => element.id == req.params.id).title,
+    artwork : art.find(element => element.id == req.params.id)
+  }
+  res.render('artworks', viewData)
+})
