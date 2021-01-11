36da31d016cd70eb94b828d890e81509ed904596
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-12 11:44:52 +1300

    step 6 of MVP completed

--- server.js
+++ server.js
@@ -20,4 +20,23 @@ const viewData = {
 
 server.get('/', (req, res) => {
   res.render('home', viewData)
+
+})
+
+server.get('/artworks/:id', (req, res) => {
+  let viewData = {
+    artwork: ""
+  }
+  if (req.params.id === '1') {
+    viewData.artwork = art.find(pic => pic.id === 1).artwork
+  } else if(req.params.id === '2') {
+    viewData.artwork = art.find(pic => pic.id === 2).artwork
+  } else if(req.params.id === '3') {
+    viewData.artwork = art.find(pic => pic.id === 3).artwork
+  }else if(req.params.id === '4') {
+    viewData.artwork = art.find(pic => pic.id === 4).artwork
+  } else {
+    res.send("No ID related to image found")
+  }
+  res.render("artworks", viewData)
 })
