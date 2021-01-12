64208be7c7acc4af17cadf42fc6691e04a7700ad
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-01-12 16:00:19 +1300

    Add homepage test.

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,3 +5,12 @@ const cheerio = require("cheerio")
 test ("tests are running", () => {
     expect(2).toBeGreaterThan(1)
 })
+
+test ("/ exists", done => {
+    request(server)
+    .get("/")
+    .end((err, res) => {
+        expect(res.status).toBe(200)
+        done()
+    })
+})
