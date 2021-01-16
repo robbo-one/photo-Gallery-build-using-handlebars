4e113b90a9e12621470e71fe9797cf03559b156e
Rosemary-Hawkes <15070793+Rosemary-Hawkes@users.noreply.github.com>
2021-01-16 16:40:36 +1300

    Displays specific artwork when page visited

--- server.js
+++ server.js
@@ -28,19 +28,16 @@ server.get ('/', (req, res) => {
 //See list of titles, artist, etc. Render goes to home. Home refers to >artwork summary partial which sees keys in art object and grabs their values (name, url, license etc) from viewData.art
 
 
-//Create route that displays selected image when "/artworks///:id" is visited 
-//1. Data (images) defined in params object
-//2. object passed as second arg to res.render
-//3. hbs template modified to use image
-//IN PROGRESS
-
-server.get ('/', (req, res) => {
-  art.find()
-
-  res.render('artworks', viewData) 
+//Route that displays image when user goes to /artworks/:id (eg /artworks/1
+//Render goes to artworks template which has {{artwork}} and gets artwork data object defined in Line 3 of the function.
+server.get ('/artworks/:id', (req, res) => {
+  const id = req.params.id
+  const artwork = art.find(art => art.id == id)
+  res.render('artworks', artwork) 
 })
 
   
+
 
 
  
